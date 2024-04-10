01430bfde3edf63c5143e42b69cb2829162e1b67
Girleide de Araujo <145593247+NathalyaKSF@users.noreply.github.com>
2024-04-10 10:30:25 -0300

    a gente mexeu nas funções

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
+//arrow function(estudar)
+//get>>pegar
+app.get('/tuma', (req, res) => {
   res.send('Hello jaciane!');
+});
+
+//post>>submeter
+//com a mesma assinatura, metódos deferentes
+app.post('/', function (req, res) {
+  console.log(req.body?.nome)
+  res.send('Got a POST request');
 });
 
 app.listen(port, () => {
