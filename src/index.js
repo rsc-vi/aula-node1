23344fc6296af5ab77b5b1a68a3723fac5a17f23
Girleide de Araujo <145593247+NathalyaKSF@users.noreply.github.com>
2024-04-18 12:17:35 -0300

    usamos app.use(express.urlencoded())

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.use(express.json());
-//app.use(express.urlencoded()); //via form
+//app.use(express.json());
+app.use(express.urlencoded()); //via form
 
 //arrow function(estudar)
 //get>>pegar
 app.get('/abagaras', (req, res) => {
-  res.send('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaAAAAAAAAAA');
+  res.send('ok...');
 });
 
 //post>>submeter e enviar
 //com a mesma assinatura, metódos deferentes
-app.post('/', function (req, res) {
-  let dados = req.body.dados;  
- 
+app.post('/salvar', function (req, res) {
+  let dados = req.body.idNome; 
+  console.log("mui mal") 
+  res.send(`hola ${dados}`)
+  // res.send(`Meus dados: ${dados.nome}; ${dados.email};${dados.telefone};${dados.endereco}`);
+  // console.log(dados)
 
-
-  res.send(`Meus dados: ${dados.nome}; ${dados.email};${dados.telefone};${dados.endereco}`);
-  console.log(dados)
 });
 
 app.listen(port, () => {
@@ -30,3 +30,4 @@ app.listen(port, () => {
 //<input id='nome' nome="nome">
 //form
 //vc consegue!!!
+// eu acho não :|
